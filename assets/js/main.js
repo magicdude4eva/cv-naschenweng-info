32b5afd628a90eb37934625880916ee2802a0749
magicdude4eva <gerd@naschenweng.info>
2021-02-13 20:11:52 +0100

    Generated site via Jekyll

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,28 +13,25 @@ function openWindow(url, width, Height, bScrollablePar) {
 	}
 }
 
+
 jQuery(document).ready(function($) {
 
-
-    /*======= Skillset *=======*/
-
-
     $('.level-bar-inner').css('width', '0');
-
+    
     $(window).on('load', function() {
 
         $('.level-bar-inner').each(function() {
-
+        
             var itemWidth = $(this).data('level');
-
+            
             $(this).animate({
                 width: itemWidth
             }, 800);
-
+            
         });
 
     });
-
-
+   
+    
 
 });
